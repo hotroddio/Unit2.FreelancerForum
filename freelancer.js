a3a811628a487ebcef976ef799b47030c0427a5e
John Rodriguez <hotroddio@gmail.com>
2023-12-03 18:53:25 -0500

    This is my final solution for Unit 19

--- freelancer.js
+++ freelancer.js
@@ -1,132 +1,72 @@
-const names = ["Alice", "Bob", "Carol"];
-const jobs = ["Writer", "Teacher", "Programmer",];
-const price = [30,50,70];
-
-const greetings = [
-    {
-        header: "Freelancer Forum"
-    }
-]
-
-const availFree = [
-    {
-        title: "Available Freelancers"
-    }
-]
-
+const names = ["Alice","Bob","Carol","Bilbo","Smeagol","Gollum"];
+const jobs = ["Writer", "Teacher", "Programmer","Night Crawler", "Thief", "Barber"];
+const price = [30,50,70,90,25,56];
 const peoples = [
 {
-    name: "Alice",
-    job: "Writer",
-    price: 30,
+    name: names[0],
+    job: jobs[0],
+    price: price[0],
 },
 {
-    name: "Bob",
-    job: "Teacher",
-    price: 50,
+    name: names[1],
+    job: jobs[1],
+    price: price[1],
 },
 {
-    name: "Carol",
-    job: "Programmer",
-    price: 70,
+    name: names[2],
+    job: jobs[2],
+    price: price[2],
 },
 ];
 
-const prices = peoples.reduce ((acc, cur) => acc=cur.price,0);
-const firstTwoPrices = price.slice(0,2);
-const firstTwoAvg = firstTwoPrices.reduce((acc, cur)=> acc + cur, 0)/ firstTwoPrices.length;
-console.log(firstTwoAvg);
-const randomPrices = (price2) => {
-    for (let i = price2.length -1; i>0; i--) {
-        const j = Math.floor(Math.random() * i);
-        const temp = price2[i];
-        price2[i] = price2[j];
-        price2[j] = temp;
-    }
+function randomPerson () {
+    const table = document.getElementById("division");
+    const rand1 = Math.floor(Math.random()*names.length);
+    console.log(rand1);
+    const people = {
+        name:names[rand1],
+        price:price[rand1],
+        job:jobs[rand1],
+    };
+    peoples.push(people);
+    console.log(table);
+    render();
 }
-const price2 = price.slice();
-randomPrices(price2);
-console.log(price2);
 
-
-console.log (prices);
-const pricesAvg = price.reduce((a,b) => a+b,0)/ price.length;
-console.log (pricesAvg);
-
-function openHeader() {
-    const table = document.getElementById("header");
-    const tableElements = greetings.map((greet) => {
-        const element = document.createElement("h1");
-            element.innerHTML=`${greet.header}`
+function render() {    
+    const table = document.getElementById("division");
+        const tableElements = peoples.map((people => {
+            const element = document.createElement("li");
+            element.innerHTML= `${people.name} who is a ${people.job} is available for $${people.price} an hour.`
         return element;
-    })
-    console.log (tableElements);
-    console.log (table);
-    table.replaceChildren(...tableElements);
-}
-setTimeout(openHeader, 2000);
+    }))
+        table.replaceChildren(...tableElements);
+    let peoplesAvg = average();
+    console.log("render", peoplesAvg);
+    firstAvg(peoplesAvg);
+};
+render();
+setInterval(randomPerson, 3000 );
+// setTimeout(render, 3000);
 
-function rendered() {
-    const table = document.getElementById("table");
-    peoples.filter((people, index) => index < 2).forEach((people) => {
-        const element = document.createElement("tr");
-            element.innerHTML=`${people.name} | ${people.job} | $${people.price}`
-            table.appendChild(element);
-    })
-    console.log (table);
-}
-setTimeout(rendered, 4000);
+function average () {
+    let priceTotal = 0
+    for (i=0; i< peoples.length; i++) {
+        console.log(peoples[i]);
+        priceTotal+=peoples[i].price;
+        console.log(peoples[i].price);
 
-function renderedTwo() {
-    const table = document.getElementById("table");
-    peoples.filter((people, index) => index === 2).forEach((people) => {
-        const element = document.createElement("tr");
-            element.innerHTML=`${people.name} | ${people.job} | $${people.price}`
-            table.appendChild(element);
-            let oldChild = table.children[2];
-            let newChild = document.createElement("tr");
-            table.replaceChild(newChild,oldChild);
-    })
-    console.log (table);
-    console.log (peoples);
-}
-setTimeout(renderedTwo, 9000);
-
-function displayAvg() {
-    const element = document.createElement("tr");
-    element.innerHTML= `The average price is $${firstTwoAvg}`
-    table.appendChild(element);
-}
-setTimeout(displayAvg, 5000);
-
-function displayAvg2() {
-    const element = document.createElement("tr");
-    element.innerHTML= `The new average price is $${pricesAvg}`
-    table.appendChild(element);
-}
-setTimeout(displayAvg2, 12000);
-
-
-try {
-    for (let i = peoples.length - 1; i>0; i--) {
-      let j = Math.floor (Math.random () * (i+1));
-      [peoples[i], peoples[j]] = [peoples[j], peoples[i]];
     }
-  } catch (error) {
-    console.error (error);
-  }
-  
-function shuffleArray(arrays) {
-    for (let i = arrays.length - 1; i>0; i--) {
-        let j = Math.floor (Math.random () * (i+1));
-        [arrays[i], arrays[j]] = [arrays[j], arrays[i]];
-       }
-       const table = document.getElementById("table");
-       peoples.filter((people, index) => index === 2).forEach((people) => {
-       const element = document.createElement("tr");
-    element.innerHTML= `${people.name} | ${people.job} | $${people.price}`
-    table.replaceChild(element)});
+    console.log(priceTotal);
+    const avg = priceTotal/ peoples.length;
+    console.log(avg.toFixed(2));
+    return avg.toFixed(2); 
 }
-const shuffledPeoples = shuffleArray(peoples);
-console.log (shuffledPeoples);
-shuffleArray();
+
+function firstAvg (average) {
+    const table = document.getElementById("table");
+    const element = document.createElement("tr");
+        element.innerHTML= `The average price is $${average}!`
+        table.replaceChildren(element);
+        console.log(division);
+}
